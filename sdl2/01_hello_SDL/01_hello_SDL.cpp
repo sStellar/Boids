e68cb0fddd8f292f8b10b397bc7dde252acb09b9
sStellar <rexstellar.pa@gmail.com>
2020-01-15 19:35:32 +0100

    You know, no one else is looking anyway

--- sdl2/01_hello_SDL/01_hello_SDL.cpp
+++ sdl2/01_hello_SDL/01_hello_SDL.cpp
@@ -3,32 +3,44 @@ and may not be redistributed without written permission.*/
 
 //Using SDL and standard IO
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 #include <stdio.h>
+#include <string>
+
+//The window we'll be rendering to
+SDL_Window* window = NULL;
+//The surface contained by the window
+SDL_Surface* screenSurface = NULL;
 
 //Screen dimension constants
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 480;
 
-int main( int argc, char* args[] )
-{
-	//The window we'll be rendering to
-	SDL_Window* window = NULL;
+void closeWindow(SDL_Window* window) {
+	//Destroy window
+	SDL_DestroyWindow( window );
 
-	//The surface contained by the window
-	SDL_Surface* screenSurface = NULL;
+	//Quit SDL subsystems
+	SDL_Quit();
+}
+
+bool initWindow(const char* window_name, int open_time_s) {
+	bool success = true;
 
 	//Initialize SDL
 	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
 	{
 		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+		success = false;
 	}
 	else
 	{
 		//Create window
-		window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+		window = SDL_CreateWindow( window_name, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
 		if( window == NULL )
 		{
 			printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+			success = false;
 		}
 		else
 		{
@@ -36,21 +48,42 @@ int main( int argc, char* args[] )
 			screenSurface = SDL_GetWindowSurface( window );
 
 			//Fill the surface white
-			SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
+			//SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
+
+
 
 			//Update the surface
 			SDL_UpdateWindowSurface( window );
 
-			//Wait two seconds
-			SDL_Delay( 10000 );
+			closeWindow(window)
 		}
 	}
+	return success;
+}
 
-	//Destroy window
-	SDL_DestroyWindow( window );
+bool printPNG(std::string img_path) {
+	bool success = true;
+	//The final optimized image
+  SDL_Surface* optimizedSurface = NULL;
 
-	//Quit SDL subsystems
-	SDL_Quit();
+	SDL_Surface* png_image = IMG_Load(img_path.c_str());
+	if( png_image == NULL ) {
+        printf( "Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError() );
+				success = false;
+  } else {
+		//Convert surface to screen format
+    optimizedSurface = SDL_ConvertSurface( png_image, gScreenSurface->format, 0 );
+    if( optimizedSurface == NULL ) {
+      printf( "Unable to optimize image %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
+			success = false;
+    }
+	}
+	return optimizedSurface;
+}
+
+int main()
+{
+	initWindow("Boids", 4);
 
 	return 0;
 }
